27cda4194a235dd5b32d618bc0119e6616b98248
YuWang(Steven) <daaiwusheng003@gmail.com>
2021-11-12 09:54:09 +0000

    compelete configurition
    can run qt and gl on mac with Clion

---
+++ cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/DependInfo.cmake
@@ -0,0 +1,92 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/src/glad.c" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "FakeGLRenderWindowRelease_autogen/include"
+  "/usr/local/Cellar/glew/2.2.0_1/include/GL"
+  "/usr/local/Cellar/glfw/3.3.4/include/GLFW"
+  "/usr/local/include/glad"
+  "/usr/local/include/KHR"
+  "/usr/local/opt/qt@5/lib/QtCore.framework"
+  "/usr/local/opt/qt@5/lib/QtCore.framework/Headers"
+  "/usr/local/opt/qt@5/./mkspecs/macx-clang"
+  "/usr/local/opt/qt@5/lib/QtWidgets.framework"
+  "/usr/local/opt/qt@5/lib/QtWidgets.framework/Headers"
+  "/usr/local/opt/qt@5/lib/QtGui.framework"
+  "/usr/local/opt/qt@5/lib/QtGui.framework/Headers"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/ArcBall.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/ArcBall.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/ArcBallWidget.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/ArcBallWidget.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/Cartesian3.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/Cartesian3.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/FakeGL.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/FakeGL.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/FakeGLRenderWidget.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/FakeGLRenderWidget.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/FakeGLRenderWindowRelease_autogen/mocs_compilation.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/FakeGLRenderWindowRelease_autogen/mocs_compilation.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/Homogeneous4.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/Homogeneous4.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/Matrix4.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/Matrix4.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/Quaternion.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/Quaternion.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/RGBAImage.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/RGBAImage.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/RGBAValue.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/RGBAValue.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/RenderController.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/RenderController.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/RenderWidget.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/RenderWidget.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/RenderWindow.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/RenderWindow.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/TexturedObject.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/TexturedObject.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/main.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5812/Assignment1/研究/FakeGLRenderWindowRelease/cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "FakeGLRenderWindowRelease_autogen/include"
+  "/usr/local/Cellar/glew/2.2.0_1/include/GL"
+  "/usr/local/Cellar/glfw/3.3.4/include/GLFW"
+  "/usr/local/include/glad"
+  "/usr/local/include/KHR"
+  "/usr/local/opt/qt@5/lib/QtCore.framework"
+  "/usr/local/opt/qt@5/lib/QtCore.framework/Headers"
+  "/usr/local/opt/qt@5/./mkspecs/macx-clang"
+  "/usr/local/opt/qt@5/lib/QtWidgets.framework"
+  "/usr/local/opt/qt@5/lib/QtWidgets.framework/Headers"
+  "/usr/local/opt/qt@5/lib/QtGui.framework"
+  "/usr/local/opt/qt@5/lib/QtGui.framework/Headers"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FakeGLRenderWindowRelease_autogen"
+  "FakeGLRenderWindowRelease_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FakeGLRenderWindowRelease_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
