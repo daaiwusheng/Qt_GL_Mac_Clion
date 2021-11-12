37fc600fbd4c899de64eee0add7c7169db8c08aa
YuWang(Steven) <daaiwusheng003@gmail.com>
2021-11-12 09:54:09 +0000

    compelete configurition
    can run qt and gl on mac with Clion

---
+++ cmake-build-debug/CMakeFiles/FakeGLRenderWindowRelease.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FakeGLRenderWindowRelease_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/FakeGLRenderWindowRelease_autogen.dir/ParseCache.txt"
+  "FakeGLRenderWindowRelease_autogen"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/ArcBall.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/ArcBallWidget.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/Cartesian3.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/FakeGL.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/FakeGLRenderWidget.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/FakeGLRenderWindowRelease_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/Homogeneous4.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/Matrix4.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/Quaternion.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/RGBAImage.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/RGBAValue.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/RenderController.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/RenderWidget.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/RenderWindow.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/TexturedObject.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/main.cpp.o"
+  "CMakeFiles/FakeGLRenderWindowRelease.dir/src/glad.c.o"
+  "FakeGLRenderWindowRelease"
+  "FakeGLRenderWindowRelease.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/FakeGLRenderWindowRelease.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
